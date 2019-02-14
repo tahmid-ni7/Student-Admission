70446c3593af5e1b88f49af114e4feda9358e2eb
Tahmid Nishat <43580743+tahmid-ni7@users.noreply.github.com>
2019-02-14 11:26:45 +0600

    First Commit

---
+++ component/menu.php
@@ -0,0 +1,8 @@
+<ul>
+    <li><a href="index.php">home</a></li>
+    <li><a href="?a=student_add">admission</a></li>
+    <li><a href="?a=view">students</a></li>
+    <li><a href="#">Attendance</a></li>
+    <li><a href="#">Payment</a></li>
+    <li><a href="">contact us</a></li>
+</ul>
